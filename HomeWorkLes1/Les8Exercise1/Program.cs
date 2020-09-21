186ed32dce69834f38817b7291b6aa0d2b8360d8
Leonid <jiehuh87@yandex.ru>
2020-09-21 23:35:17 +0300

    Урок 8

---
+++ HomeWorkLes1/Les8Exercise1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Les8Exercise1
+{
+    //Ронжин Л.
+    //1. С помощью рефлексии выведите все свойства структуры DateTime
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime();
+            var properties = dateTime.GetType().GetProperties();
+            Console.WriteLine("Свойства структуры DateTime:");
+            int i = 0;
+            foreach (var item in properties)
+            {
+                Console.WriteLine($"{++i}. {item.Name}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWorkLes1/Les8Exercise5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Les8Exercise5
+{
+    //Ронжин Л.
+    //** Написать программу-преобразователь из CSV в XML-файл с информацией о студентах(6 урок).
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
